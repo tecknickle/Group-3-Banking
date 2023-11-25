7e815d4662ddf7a7496f3955a45689b22328e834
David Kromka <46538919+DKromka@users.noreply.github.com>
2023-11-25 14:35:04 -0800

    test server.java

--- src/Server.java
+++ src/Server.java
@@ -1,11 +1,14 @@
 
 public class Server {
 	public static void main(String[] args) {
-		System.out.println("Hello Group 3");
+		
 	}
 	void DavidsCode() {
 	System.out.println("MY CODE");
 	}
+	
+	
+	
 	void otherFun() {
 		
 	}
