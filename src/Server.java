481aecbef84094c432427f6e3a764de697217119
David Kromka <46538919+DKromka@users.noreply.github.com>
2023-11-29 19:13:01 -0800

    added code to to verify users in server.java. WIP

--- src/Server.java
+++ src/Server.java
@@ -5,18 +5,35 @@ import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class Server {
 	//Can you put input/outputStreams to send message objects
 	//back and forth server to client... 
-		
+	static private HashMap <String,User> Users;
+	static private HashMap <String,bankAccount> Accounts;
+	static private HashMap <String,log> Logs;
+	
+	static private int b = 0;
+	
+	Server(){
+		Users = new HashMap<String,User>();
+		Accounts = new HashMap<String,bankAccount>();
+	}
 	public static void main(String[] args) {
 		ServerSocket server= null;
 		try {
+			/*****Testing variable access*****/
+			System.out.println("1: " + b);
+			test t = new test();
+			t.change();
+			System.out.println("2: " + b);
+			t.change(13);
+			System.out.println("1: " + b);
 			//Create a ServerSock on socket:4591
 	        server = new ServerSocket(4591);
 	        server.setReuseAddress(true);
-	        
+	        /*********************************/
 			while(true) {
 		        System.out.println("ServerSocket awaiting connections...");
 
@@ -43,22 +60,107 @@ public class Server {
 		}
 	}
 	
+	private static class test{
+		int a;
+		test(){
+			a = 4;
+		}
+		public void change(int a) {
+			b = a;
+		}
+		public void change() {
+			b = a;
+		}
+	}
+	
 	private static class ClientHandler implements Runnable {
 		private final Socket clientSocket;
-		private Message msgIO;
+		private Message msg;
 		private OutputStream outputStream;
 		private InputStream inputStream;
 		private ObjectOutputStream out;
 		private ObjectInputStream in;
+		private String currClient;
+		private boolean loggedIN;
+		private boolean Teller;
+		private Server parent;
 		//Constructor
 		public ClientHandler(Socket socket)  throws IOException, ClassNotFoundException
 		{
+			loggedIN = false;
+			Teller = false;
+			
 			clientSocket = socket;
+			
+	        //get the input stream from the connected socket
+	        outputStream = clientSocket.getOutputStream();
+	        inputStream = clientSocket.getInputStream();
+
+	        //create a ObjectInputStream so we can read data from it.
+	        out = new ObjectOutputStream(outputStream);
+	        in = new ObjectInputStream(inputStream);
 		}
 
 		public void run()
 		{
-			
+			try {
+				
+				verifyLogin();
+				
+				String inputStr;
+				while(loggedIN) {
+					msg = (Message)in.readObject();	
+				}
+				
+		        clientSocket.close();				
+			}
+			catch (IOException | ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			finally {
+				try {
+					if (out != null) {
+						out.close();
+					}
+					if (in != null) {
+						in.close();
+						clientSocket.close();
+					}
+				}
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		void verifyLogin() {
+			String[] input;
+			String username, password;
+			while(!loggedIN) {
+				try {
+					msg = (Message)in.readObject();
+					if(msg.type == MessageType.Login) {
+						input = msg.getData().split("\n");
+						username = input[0];
+						password = input[1];
+						User u = Users.get(input[0]);
+						if(u.verifyUser(username,password)) {
+							loggedIN = true;
+							// send out approval message
+						}
+						else {
+							//send out disapproval message
+						}
+					}
+					else {
+						loggedIN = false;
+						break;
+					}
+				}
+				catch (IOException | ClassNotFoundException e) {
+				e.printStackTrace();
+				}
+			}
 		}
 	}
 }
