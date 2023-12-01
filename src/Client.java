49b805228dd8aaf73f449d77901e0ff65f84cfcd
Daniel Leal <dleal@horizon.csueastbay.edu>
2023-11-30 18:34:43 -0800

    client class

--- src/Client.java
+++ src/Client.java
@@ -4,46 +4,78 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 import java.util.Vector;
 import java.util.logging.Handler;
 
 
 public class Client extends User{
-//create socket for 2 way communication outputStream/inputStream
+	
+
+
+
+
+	//create socket for 2 way communication outputStream/inputStream
 	private Socket socket;
-	private ClientHandler currHandler;
-	private String ID;
+	private ObjectOutputStream outstream;
+	private ObjectInputStream instream;
+	private Vector<String> accounts;
+	private static String name;
+	private static String password;
+	private static boolean teller;
+	
+	ClientHandler currHandler;
+	
+
+	
 	
 	
 	//main
 		public static void main(String[] args) throws IOException, ClassNotFoundException {
-			info();
-			//verify (user class)
-			verifyUser();
-					
-		}
-		
-	
-	//constructor
-	public Client(String server, int portNumber) throws IOException {
-		//make socket and 2 way connections. 
-		socket = new Socket(server, portNumber);
-	    outstream= new ObjectOutputStream(socket.getOutputStream());
-	    instream = new ObjectInputStream(socket.getInputStream());
-	}
+			//Create a ServerSock on socket:4591
 
-	//NOTE: ID = string with what? 
-	private static boolean addAccount(String id) {
+			info();
+			//make a client
+			Client client = null;
+			client = new Client("localhost", 4591);
+
+			//send login request to server
+			Message message;
+			MessageType mt = MessageType.LOGIN_REQ;
+		    message = new Message(mt, "", 0);
+		    client.outstream.writeObject(message);
+		    
+		    //receive message server message
+		    message = (Message) client.instream.readObject();
+		    
+		    //if type = success prompt the user with message
+		    if (message.getType().equals("SUCCESS")) {
+		    	//client connected to server
+				System.out.println("This is the Client Class");
+		    	
+		    }
+		    
+				    
+		}
+
+ 
+    
+
+
+
+	
+	public boolean addAccount(String id) {
 		//STUB: add a bank account to user with ID "id"
 		return false;
 
 		
 	}
 
-	//NOTE: ID = string with what? 
-	private static boolean removeAccount(String id) {
+	public boolean removeAccount(String id) {
 		//STUB: remove an account from user with ID "id"
 		return false;
 
@@ -55,8 +87,8 @@ public class Client extends User{
 	//getAccount method for vectors 
 		Vector<String> getAccounts(String accountID) throws IOException, ClassNotFoundException{
 			//create message object to get accountID info 
-	        MessageType mt = MessageType.getInfo;
-		    Message message = new Message(0,mt, "",0);
+	        MessageType mt = MessageType.ACCOUNT_INFO;
+		    Message message = new Message(mt, "",0);
 			//send message to server
             outstream.writeObject(message);
             outstream.flush();
@@ -110,10 +142,24 @@ public class Client extends User{
 	//Set Client Handler
 	public  void setHandler() {
 		//stub
+		
 	}
 	
 	
+	public Client(String n, String p, boolean isTeller) {
+		super(n, p, isTeller);
+	}
 	
+	//connect to server
+	public Client(String server, int numPort) throws UnknownHostException, IOException {
+		super(name, password, teller);
+
+		socket = new Socket(server, numPort);
+	    outstream= new ObjectOutputStream(socket.getOutputStream());
+	    instream = new ObjectInputStream(socket.getInputStream());
+	}
+
+
 
 	//print a message
 	private static void info() {
